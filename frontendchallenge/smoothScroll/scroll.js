195ae9fa27a6dc1de5693e084649b33d8a945f3f
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-02-26 18:35:07 +0530

    added smoothScroll

---
+++ frontendchallenge/smoothScroll/scroll.js
@@ -0,0 +1,30 @@
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+  // ease function copied from internet, gizma.com/easing/
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+var section1 = document.querySelector(".section1");
+section1.addEventListener("click", function () {
+  smoothScroll(".section2", 1000);
+});
