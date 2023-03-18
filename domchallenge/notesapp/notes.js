16da9bd405faaad2f4f6a1e1cf9fc78483959b58
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-03-19 02:24:01 +0530

    addedCompleteNotesApp

--- domchallenge/notesapp/notes.js
+++ domchallenge/notesapp/notes.js
@@ -1,5 +1,9 @@
-const getBtn = document.querySelector(".get");
+const saveBtn = document.querySelector(".save");
 const form = document.querySelector(".form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
 
 //===================
 
@@ -13,17 +17,41 @@ function popupClose() {
   document.getElementById("overlay").style.display = "none";
 }
 
-// `form.addEventListener("click", function (e) {
-//   e.preventDefault();
-// });`
+saveBtn.addEventListener("click", function (e) {
+  let addText = document.getElementById("note");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addText.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addText.value = " ";
 
-function addNote() {
-  var newElement = document.createElement("p");
-  newElement.innerHTML = "CONTENTS";
-  localStorage.setItem("item", JSON.stringify(newElement));
-  // document.getElementById("id").appendChild(newElement);
+  showNotes();
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = " ";
+  notesObj.forEach(function (element, index) {
+    html += `
+    <div class = "card">
+    <div class = "card-body">
+    <p class= "card-text">${element + 1}</p>
+    </div>
+    </div>
+    `;
+  });
+  let notesElm = document.getElementById("id");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  }
 }
-
-document
-  .getElementById("id")
-  .appendChild(JSON.parse(localStorage.getItem("item")));
