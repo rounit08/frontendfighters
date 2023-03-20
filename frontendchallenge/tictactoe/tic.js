6572f6ba6a69f488dd244ec9e362cab41fdae3c1
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-03-21 00:14:32 +0530

    addedTicTacToe

--- frontendchallenge/tictactoe/tic.js
+++ frontendchallenge/tictactoe/tic.js
@@ -4,7 +4,10 @@ const changeTurn = () => {
   return turn === "X" ? "0" : "X";
 };
 
+let gameOver = false;
+
 const winning = () => {
+  let boxtexts = document.getElementsByClassName("boxtext");
   let wins = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,7 +15,20 @@ const winning = () => {
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
+  wins.forEach((e) => {
+    if (
+      boxtexts[e[0]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[2]].innerText === boxtexts[e[1]].innerText &&
+      boxtexts[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtexts[e[0]].innerText + " WON";
+      gameOver = true;
+    }
+  });
 };
 
 let boxes = document.getElementsByClassName("box");
@@ -23,6 +39,19 @@ Array.from(boxes).forEach((element) => {
     if (boxtxt.innerText === "") boxtxt.innerText = turn;
     turn = changeTurn();
     winning();
-    document.getElementsByClassName("info")[0].innerText = "Turn for" + turn;
+    if (!gameOver) {
+      document.getElementsByClassName("info")[0].innerText =
+        "Turn for" + " " + turn;
+    }
   });
 });
+
+const resetButton = document.getElementById("resetButton");
+
+function resetGame() {
+  let boxText = document.querySelectorAll(".boxtext");
+  Array.from(boxText).forEach((element) => {
+    element.innerText = " ";
+  });
+}
+resetButton.addEventListener("click", resetGame);
