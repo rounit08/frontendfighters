941830f9c968a66819aa1b3fa4330be30a0b9931
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-03-20 23:51:42 +0530

    tictac

--- frontendchallenge/tictactoe/tic.js
+++ frontendchallenge/tictactoe/tic.js
@@ -5,7 +5,14 @@ const changeTurn = () => {
 };
 
 const winning = () => {
-  let wins = [[], [], [], []];
+  let wins = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
 };
 
 let boxes = document.getElementsByClassName("box");
