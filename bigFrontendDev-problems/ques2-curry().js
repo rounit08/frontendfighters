5fc374a4de743853563794cc63df2d9117f27bd0
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-03-24 00:09:45 +0530

    added-BFE-solution-2

---
+++ bigFrontendDev-problems/ques2-curry().js
@@ -0,0 +1,59 @@
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`;
+};
+
+const curriedJoin = curry(join);
+const _ = curry.placeholder;
+
+console.log(curriedJoin(1, 2, 3)); // '1_2_3'
+console.log(curriedJoin(_, 2)(1, 3)); // '1_2_3'
+console.log(curriedJoin(_, _, _)(1)(_, 3)(2)); // '1_2_3'
+
+function curry(func) {
+  return function curried(...args) {
+    // we need to return a function to make it curry-able.
+
+    // 1. If the arguments are extra then eliminate them
+    // we don't want to pass 6 arguments when the expected is 3.
+    // it will interfere with our placeholder logic
+    const sanitizedArgs = args.slice(0, func.length);
+
+    // see if placeholder is available in arguments
+    const hasPlaceholder = sanitizedArgs.some(
+      (arg) => arg == curry.placeholder
+    );
+
+    // if no placeholder and arguements are equal to what expected then it is normal function call
+    if (!hasPlaceholder && sanitizedArgs.length == func.length) {
+      return func.apply(this, sanitizedArgs);
+    }
+
+    // else we need to replace placeholders with actual values
+    // we call helper function `mergeArgs` for this
+    // we pass first and next arguments to helper function
+    return function next(...nextArgs) {
+      return curried.apply(this, mergeArgs(sanitizedArgs, nextArgs));
+    };
+  };
+}
+
+function mergeArgs(args, nextArgs) {
+  let result = [];
+
+  // iterate over args (because we need to replace from it)
+  // in each iteration, if we find element == curry.placeholder
+  // then we replace that placeholder with first element from nextArgs
+  // else we put current element
+  args.forEach((arg, idx) => {
+    if (arg == curry.placeholder) {
+      result.push(nextArgs.shift());
+    } else {
+      result.push(arg);
+    }
+  });
+
+  // we merge both, because there might be chance that args < nextArgs
+  return [...result, ...nextArgs];
+}
+
+curry.placeholder = Symbol();
