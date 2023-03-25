d31c778373a81cd97eddad9a721e993f3ae833f6
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-03-25 22:16:30 +0530

    arrayFlatmethod-part1

---
+++ bigFrontendDev-problems/ques3-flat().js
@@ -0,0 +1,39 @@
+//======understanding the concept======
+// The flat() method creates a new array with all sub-array
+// elements concatenated into it recursively up to the specified depth.
+
+const arr1 = [0, 1, 2, [3, 4]];
+
+// console.log(arr1.flat());
+//  output: Array [0, 1, 2, 3, 4]
+
+const arr2 = [0, 1, 2, [[[3, 4]]]];
+
+// console.log(arr2.flat(2));
+//  output: Array [0, 1, 2, Array [3, 4]]
+// console.log(arr2.flat(3));
+//  output: Array [0, 1, 2, 3, 4]
+
+const arr = [1, 2, 3, [4, [5, [6, [7, [8, [9, [10]]]]]]]];
+//          -------------2---3-------4---
+// console.log(arr.flat(7));
+
+//question 3, BFE.dev
+// There is already Array.prototype.flat() in JavaScript (ES2019), which reduces the nesting of Array.
+
+// Could you manage to implement your own one?
+
+// Here is an example to illustrate
+
+// const arr = [1, [2], [3, [4]]];
+
+// flat(arr)
+// // [1, 2, 3, [4]]
+
+// flat(arr, 1)
+// // [1, 2, 3, [4]]
+
+// flat(arr, 2)
+// // [1, 2, 3, 4]
+
+//building logic
