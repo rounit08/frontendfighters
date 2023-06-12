7b64dd4b8a9868a025c23d49c925890aeb5ed89f
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-06-12 23:32:12 +0530

    added-machinecodingroundstructure

---
+++ Frontend-Interview/machinecodingroundreactapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./questions/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend-Interview/machinecodingroundreactapp/src/questions/counter/Counter.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Counter.css";
+
+const Counter = () => {
+  return <div className="counter">Hello</div>;
+};
+
+export default Counter;
