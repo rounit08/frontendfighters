4141ac244d9a726fb015ee43ea9958b1660bbfad
Rounit Ranjan Sinha <rounitrsinha@gmail.com>
2023-06-13 20:38:21 +0530

    dark-lighttheme

--- Frontend-Interview/machinecodingroundreactapp/src/questions/dark/UseTheme.jsx
+++ Frontend-Interview/machinecodingroundreactapp/src/questions/dark/UseTheme.jsx
@@ -5,13 +5,6 @@ const useTheme = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
 
   useEffect(() => {
-    // Update the CSS file based on the current theme
-    const cssLink = document.getElementById("theme-css");
-    if (cssLink) {
-      cssLink.href = `/path/to/${theme}-theme.css`;
-    }
-
-    // Store the current theme in local storage
     localStorage.setItem("theme", theme);
   }, [theme]);
 
